d9aad53ef6fe629b831effd1fc7b51fe408c08d0
Jshubham986 <jshubham986@gmail.com>
2023-07-15 23:42:29 +0530

    second commit

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -16,7 +16,8 @@ import {
     figma,
     docker,
     meta,
-    starbucks,
+    OCTO,
+    
     tesla,
     shopify,
     carrent,
@@ -116,24 +117,24 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
+      title: "Intern",
+      company_name: "Octoedge Technologies Pvt Ltd",
+      icon: OCTO,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "May 2021 - Nov 2021",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Provided Support and Maintainance for existing websites"
       ],
     },
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
+      title: "Front End Developer",
+      company_name: "Octoedge Technologies Pvt Ltd",
+      icon: OCTO,
       iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      date: "Dec 2021 - Aug 2022",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -142,31 +143,18 @@ import {
       ],
     },
     {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
+      title: "MERN Stack Developer",
+      company_name: "Octoedge Technologies Pvt Ltd",
+      icon: OCTO,
       iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
+      date: "Sep 2022 - Present",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
       ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
+    }
   ];
   
   const testimonials = [
